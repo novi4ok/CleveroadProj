3cbdcf4500923d0ed2e47cd6f9d721444c073525
yurivyunik2 <jurij_vyunik2@mail.ru>
2016-10-22 19:20:15 +0300

    no message

--- js/modules/appMainModule.js
+++ js/modules/appMainModule.js
@@ -1,6 +1,6 @@
 ﻿(function (angular) {
   'use strict';
-  var appMain = angular.module('appMain', ['ngRoute', 'loginModule', 'userProfileModule']);
+  var appMain = angular.module('appMain', ['ngRoute', 'loginModule', 'userProfileModule', 'ui.bootstrap']);
   
   // config
   appMain.config(["$routeProvider", function ($routeProvider) {
@@ -48,17 +48,58 @@
     '$scope', '$window', 'userProfile', function ($scope, $window, userProfile) {
       $scope.userInfo = userProfile.getUserInfo();
 
-      $scope.goodsList = [];
+      $scope.makeGoods = function () {
+        $scope.goodsList = [];
+        for (var i = 0; i < 1000; i++) {
+          $scope.goodsList.push(
+          {
+            id: i,
+            name: 'name' + i,
+            price: i * 1000.23,
+            description: 'description' + i
+          });
+        }
+      };
+      $scope.makeGoods();
 
-      for (var i = 0; i < 100; i++) {
-        $scope.goodsList.push(
-        {
-          id: i,
-          name: 'name' + i,
-          price: i * 1000.23,
+      $scope.filterCountList = [10, 20, 50];
+      $scope.filterCount = $scope.filterCountList[0];
 
-        });
+      $scope.filteredGoodsList = []
+     , $scope.currentPage = 1
+     , $scope.maxSize = 5;
+
+      $scope.numPages2 = function () {
+        return Math.ceil($scope.goodsList.length / parseInt($scope.filterCount));
+      };
+      $scope.numPages = $scope.numPages2();
+
+      $scope.$watch('currentPage + filterCount', function () {
+        var filterCount = parseInt($scope.filterCount);
+        var begin = (($scope.currentPage - 1) * filterCount)
+        , end = begin + filterCount;
+
+        $scope.filteredGoodsList = $scope.goodsList.slice(begin, end);
+        $scope.numPages = $scope.numPages2();
+      });
+
+      $scope.detailsClick = function (event, goods) {
+        goods.isShowDetails = !goods.isShowDetails;
+
+        var detailsElem = angular.element(event.currentTarget);
+        var trParentElem = detailsElem.parent().parent();
+        var tableParentElem = trParentElem.parent();
+        angular.element(tableParentElem[0].querySelector(".trDetailsArea")).remove();
+        angular.element(tableParentElem[0].querySelectorAll(".tdDetailsOpen")).text("Details");
+        if (goods.isShowDetails) {
+          detailsElem.text("Hide");
+          detailsElem.addClass("tdDetailsOpen");
+          trParentElem.after("<tr class='trDetailsArea'><td colspan='4'>" + goods.description + "</td></tr>");
+        } else {
+          //angular.element(tableParentElem.querySelector("tdDetailsOpen")).text("Details");
+        }
       }
+
     }
   ]);
 
@@ -69,6 +110,7 @@
       return parseFloat(value).toFixed(2).toString().replace('.', ',');
     };
   });
+
 
   appMain.factory('userInfo', function () {
     var taskCollectionObj = {
